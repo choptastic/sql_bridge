b8337d2e44766bd5af3ae72c4e2598df51eec0bb
Jesse Gumm <gumm@sigma-star.com>
2022-04-13 14:58:33 -0500

    Add support for getting db from config

--- src/sql_bridge.erl
+++ src/sql_bridge.erl
@@ -149,7 +149,13 @@ connect() ->
 -spec connect(db()) -> db().
 % @doc establishes a connection to the named database.
 connect(DB) when is_atom(DB) ->
-    ok = ?ADAPTER:connect(DB, ?USER, ?PASS, ?HOST, ?PORT),
+    Host = case ?HOST of
+        {Mod, Fun} ->
+            Mod:Fun();
+        X ->
+            X
+    end,
+    ok = ?ADAPTER:connect(DB, ?USER, ?PASS, Host, ?PORT),
     DB.
 
 -spec pl(Table :: table(), Data :: proplist_or_map()) -> insert_id().
