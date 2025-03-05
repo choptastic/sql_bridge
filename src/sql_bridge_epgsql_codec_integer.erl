e4a22529e41b6d4ac1ac573ec186a33407d91ae6
Jesse Gumm <gumm@sigma-star.com>
2025-03-05 14:15:42 -0600

    In postgres, add null fallback if integer encoding fails

--- src/sql_bridge_epgsql_codec_integer.erl
+++ src/sql_bridge_epgsql_codec_integer.erl
@@ -1,5 +1,6 @@
 -module(sql_bridge_epgsql_codec_integer).
 -behaviour(epgsql_codec).
+-include_lib("epgsql/include/protocol.hrl").
 
 -export([init/2, names/0, encode/3, decode/3, decode_text/3]).
 
@@ -12,8 +13,13 @@ names() ->
     ?MAIN_MOD:names().
 
 encode(Data, Type, State) ->
-    N = normalize_int(Data),
-    ?MAIN_MOD:encode(N, Type, State).
+    try normalize_int(Data) of
+        N ->  ?MAIN_MOD:encode(N, Type, State)
+    catch _:_ -> 
+        %% Hardcode the NULL term here (not sure the better way)
+        logger:warning("~p: Failed to convert term (~p) to integer. Convering to 'null'", [?MODULE, Data]),
+        << -1:?int32>>
+    end.
 
 decode(Data, Type, State) ->
     ?MAIN_MOD:decode(Data, Type, State).
